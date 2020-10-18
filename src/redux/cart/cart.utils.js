a8987fda727fc53209e7371d852ba6ef227fcb91
rohitvernekar7 <rohitvernekar7@yahoo.com>
2020-10-18 22:52:51 +0530

    spell check

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,4 @@
-export const addItemTocart = (cartItems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
         cartItem => cartItem.id === cartItemToAdd.id
     );
